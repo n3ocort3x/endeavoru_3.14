940a143c747bbd8cefac6fc9c93e295ff8f8e0e2
n3ocort3x <root@n3ocort3x-tuxbook.(none)>
2013-02-07 11:44:20 +0100

    generated wlan stuff, can be romoved

--- drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
+++ drivers/net/wireless/compat-wireless_R5.SP2.03/include/linux/compat_autoconf.h
@@ -2,7 +2,7 @@
 #define COMPAT_AUTOCONF_INCLUDED
 /*
  * Automatically generated C config: don't edit
- * Tue Feb  5 20:35:32 CET 2013 
+ * Wed Feb  6 19:29:05 CET 2013 
  * compat-wireless-2.6: ol_R5.00.14
  * linux-2.6: ol_R5.00.21-23-g618c7e5
  */
