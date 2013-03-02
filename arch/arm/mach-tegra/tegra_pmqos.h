2ec0ab2abc81ec50634e34acda950deef0bd01c4
n3ocort3x <root@n3ocort3x-tuxbook.(none)>
2013-03-02 17:57:44 +0100

    XMISTER: stay on lower freqs

--- arch/arm/mach-tegra/tegra_pmqos.h
+++ arch/arm/mach-tegra/tegra_pmqos.h
@@ -26,7 +26,7 @@
  */
 
 /* in kHz */
-#define BOOST_CPU_FREQ_MIN	1700000
+#define BOOST_CPU_FREQ_MIN	475000
 #define CAP_CPU_FREQ_MAX	475000
 #define T3_CPU_MIN_FREQ     51000
 #define T3_LP_MAX_FREQ     	475000
