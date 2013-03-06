a866d2512744a679651f3cfd5c9fa8001e8e1756
n3ocort3x <root@n3ocort3x-tuxbook.(none)>
2013-03-06 22:41:59 +0100

    add intellidemand governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -373,6 +373,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_TOUCHDEMAND)
 extern struct cpufreq_governor cpufreq_gov_touchdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_touchdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellidemand)
 #endif
 
 
